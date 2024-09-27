bb2c6c50b6e35eb1b0b6edca4174c9f048893c4b
Rakshendr <rakshendrakrchaudhari@gmail.com>
2024-09-27 14:38:00 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,24 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
-        
-        ans = 0
-        
-        for i in range(len(s)):
-            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
-                ans -= m[s[i]]
-            else:
-                ans += m[s[i]]
-        
-        return ans
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
 
 import unittest

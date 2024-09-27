e9513e2d71289bfc0e1af67314b0aa2de11f1a3f
Rakshendr <rakshendrakrchaudhari@gmail.com>
2024-09-27 14:49:19 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -54,5 +54,4 @@ class Solution(object):
                     (c == ']' and stack[-1] != '['):
                     return False # the string is not valid, so return false
                 stack.pop() # otherwise, pop the opening bracket from the stack
-        return not stack # if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                         # so the string is valid, otherwise, there are unmatched opening brackets, so return false
+        return not stack 
